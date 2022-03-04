332794c0ef37aa023bd2f7317ddb13ae332c133b
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2022-03-04 15:49:36 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '@styles/globals.css'
+import Head from 'next/head'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>Pluto - NeoBanking App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <Component {...pageProps} /> 
+    </>
+    )
 }
 
 export default Application
