945034d5a9efa68cf2f614d4a54b66a370855c86
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2022-03-05 01:30:17 +0530

    added rudimentary Navbar

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,189 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Avatar, Stack } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+
+const drawerWidth = 240;
+
+const theme = createTheme({
+    //createTheme is a function that takes in a theme object and returns a ThemeProvider
+    palette: {
+      primary: {
+        light: "#ffffff",
+        main: "#ffffff",
+        dark: "#ffffff",
+        contrastText: "#000000"
+      },
+      secondary: {
+        light: "#ffffff",
+        main: "#ffffff",
+        dark: "#ffffff",
+        contrastText: "#000000"
+      }
+    }
+  });
+  
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function MiniDrawer() {
+  const isMobileView = useMediaQuery('(max-width:600px)');
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+     
+        <ThemeProvider theme={theme}>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+       { !isMobileView &&  <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton> }
+          <Stack direction = "row" >
+        <IconButton disableRipple size = "small">
+          <Avatar src = '/logo.png' />
+          </IconButton> 
+          <IconButton disableRipple size = "small">
+          <Typography variant="h5" noWrap component="div" color = "#00308F"  style = {{fontFamily : "Bahnschrift"}}>
+           Pluto
+          </Typography>
+            </IconButton>
+            </Stack>
+        </Toolbar>
+      </AppBar>
+    { !isMobileView &&   <Drawer variant="permanent" color = "white" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+        <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary = "home" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          
+        </List>
+       
+      </Drawer> }
+      
+      </Box>
+        </ThemeProvider>
+  
+  );
+}
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import BottomNavBar from '@components/BottomNavigation/BottomNavBar'
-
+import NavBar from '@components/NavBar/NavBar'
 
 function DashBoard() {
   return (
     <>
+        <NavBar />
         <BottomNavBar />
     </>
   )
