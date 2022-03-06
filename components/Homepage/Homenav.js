b247d6c4a8bc1a534c44fc3c9e1df15b93a8421c
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-03-06 08:45:35 +0530

    .

--- components/Homepage/Homenav.js
+++ components/Homepage/Homenav.js
@@ -2,23 +2,28 @@ import React from "react";
 import styles from "./Homenav.module.css";
 import Link from "next/link";
 function Homenav() {
-  return (
-    <div className={styles.nav}>
-      <div className={styles.nav__left}>
-        <h2>Pluto</h2>
-      </div>
-      <div className={styles.nav__right}>
-        <ul>
-          <li>Benefits</li>
-          <li>Faq</li>
-          <li>
-            <span>Login</span>
-          </li>
-        </ul>
-        <div className={styles.dwnld}>Download Now</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.nav}>
+            <div className={styles.nav__left}>
+                <h2>Pluto</h2>
+            </div>
+            <div className={styles.nav__right}>
+                <ul>
+                    <li>Benefits</li>
+                    <li>Faq</li>
+                    <li>
+                        {/* <Link href="/createAccount"> */}
+                        <span>
+
+                            Login
+                        </span>
+                        {/* </Link> */}
+                    </li>
+                </ul>
+                <div className={styles.dwnld}>Download Now</div>
+            </div>
+        </div>
+    );
 }
 
 export default Homenav;
